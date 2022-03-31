cb03dbf5486a2d0f1c17e8cef08ba328688f33cc
cristsr <styven21121@gmail.com>
2022-03-30 21:00:51 -0500

    feat: log connections

--- src/app/stream/gateway/stream.gateway.ts
+++ src/app/stream/gateway/stream.gateway.ts
@@ -7,17 +7,21 @@ import { Server, Socket } from 'socket.io';
 import { StreamRes } from 'app/stream/dtos';
 import { OnEvent } from '@nestjs/event-emitter';
 import { StreamEvents } from 'app/stream/constants';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   namespace: 'stream',
 })
 export class StreamGateway implements OnGatewayConnection {
+  private logger = new Logger(StreamGateway.name);
+
   @WebSocketServer()
   server: Server;
 
   private onlineStreams = new Map<string, StreamRes>();
 
   handleConnection(client: Socket) {
+    this.logger.log('New client connected ' + client.id);
     const streams = this.getStreams();
     client.emit('streams', streams);
   }
